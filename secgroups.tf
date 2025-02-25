d979c204bdf355c0dd393044ee85f9da705bca54
NIRAJ <patel.niraju@northeastern.edu>
2025-02-25 03:09:36 -0500

    adding new file for ec2

---
+++ secgroups.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "app_security_group" {
+  name   = "app-security-group"
+  vpc_id = aws_vpc.csye6225_vpc.id # Ensure this is the VPC you created with Terraform
+
+  # Ingress rules for allowing incoming traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS access from anywhere
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow app access from anywhere
+  }
+
+  # Egress rule: Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
