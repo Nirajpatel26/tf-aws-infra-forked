ebb8ee2ef52c2d7c1f7bf6b01ef1bb4fad59430f
NIRAJ <patel.niraju@northeastern.edu>
2025-04-02 21:01:34 -0400

    updating sec group fmt

--- secgroups.tf
+++ secgroups.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "load_balancer_sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] 
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -84,7 +84,7 @@ resource "aws_security_group" "load_balancer_sg" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] 
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
